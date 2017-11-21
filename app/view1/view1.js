3304760527f69ca270fdc9efa69f14395959da09
Timothy <tabradford@liberty.edu>
2017-11-21 07:51:35 -0500

    Initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,108 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
+
+    .controller('View1Ctrl', ['$http', '$scope', function ($http, $scope) {
+
+        $scope.checkboxModel = {
+            remember: true
+        };
+
+        $scope.hasTried = false;
+        $scope.loading = false;
+        $scope.pushHandle = {message: ""};
+        $scope.theForm = {
+            authKey:'',
+            topic: 'all',
+            title: '',
+            body:''
+        };
+
+        var dataPayload = {
+            "to": "/topics/" + $scope.theForm.topic,
+            "content_available": true,
+            "priority": "high",
+            "notification": {
+                "title": $scope.theForm.title,
+                "body": $scope.theForm.body,
+                "sound": "default",
+                "click_action": "FCM_PLUGIN_ACTIVITY",
+                "icon": "lu_today_icon_android"
+            },
+            "data": {
+                "title": "Title test",
+                "msg": "Text of the test",
+                "code": 2
+            }
+        };
+
+        var req = {
+            method: 'POST',
+            url: 'https://fcm.googleapis.com/fcm/send',
+            headers: {
+                'Authorization': 'key=' + $scope.theForm.authKey,
+                'Content-Type': 'application/json'
+            },
+            data: dataPayload
+        };
+
+
+
+        $scope.submit = function (formData) {
+
+             dataPayload = {
+                "to": "/topics/" + $scope.theForm.topic,
+                "content_available": true,
+                "priority": "high",
+                "notification": {
+                    "title": $scope.theForm.title,
+                    "body": $scope.theForm.body,
+                    "sound": "default",
+                    "click_action": "FCM_PLUGIN_ACTIVITY",
+                    "icon": "lu_today_icon_android"
+                },
+                "data": {
+                    "title": "Title test",
+                    "msg": "Text of the test",
+                    "code": 2
+                }
+            };
+
+             req = {
+                method: 'POST',
+                url: 'https://fcm.googleapis.com/fcm/send',
+                headers: {
+                    'Authorization': 'key=' + $scope.theForm.authKey,
+                    'Content-Type': 'application/json'
+                },
+                data: dataPayload
+            };
+
+
+
+            console.log(req);
+            $scope.hasTried = true;
+            if (formData.$valid) {
+                $scope.loading = true;
+                $http(req).then(function (success) {
+                    $scope.loading = false;
+                 console.log(success);
+                    $scope.pushHandle.message = "Push Successful";
+                    $scope.pushHandle.color = "green";
+                }, function (error) {
+                    $scope.loading = false;
+                    console.log(error);
+                    $scope.pushHandle.message = "The following Error Occurred:" + JSON.stringify(error);
+                    $scope.pushHandle.color = "red";
+                });
+            }
+        };
+
+    }]);
